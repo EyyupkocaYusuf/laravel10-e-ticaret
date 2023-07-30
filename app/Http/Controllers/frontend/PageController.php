fa7ba09f73466faedc4693fe341937f122c12bae
EyyupkocaYusuf <EyyupkocaYusuf@gmail.com>
2023-07-30 13:14:44 +0300

    c36 breadcrumb olusturuldu. breadcrumblar dinamik hale getirildi

--- app/Http/Controllers/frontend/PageController.php
+++ app/Http/Controllers/frontend/PageController.php
@@ -14,18 +14,42 @@ class PageController extends Controller
     public function urunler(Request $request,$slug=null)
     {
         $category = request()->segment(1) ?? null ;
-
         $sizes = !empty($request->size) ? explode(',', $request->size) : null;
-
         $colors = !empty($request->color) ? explode(',', $request->color) : null;
-
 
         $startprice = $request->min ?? null;
         $endprice = $request->max ?? null;
-
         $order = $request->order ?? 'slug';
-
         $sort = $request->sort ?? 'desc';
+
+        $anakategori = null;
+        $altkategori = null;
+        if(!empty($category) && empty($slug)) {
+            $anakategori = Category::whereSlug($category)->first();
+        }else if(!empty($category) && !empty($slug)) {
+            $anakategori = Category::whereSlug($category)->first();
+            $altkategori = Category::whereSlug($slug)->first();
+        }
+
+        $breadcrumb = [
+            'sayfalar' => [
+
+            ],
+            'active' => 'Ürünler',
+        ];
+
+        if(!empty($anakategori) && empty($altkategori)) {
+
+            $breadcrumb['active'] = $anakategori->name ?? '';
+        }
+
+        if(!empty($altkategori)) {
+            $breadcrumb['sayfalar'][] = [
+                'link' => route($anakategori->slug.'urunler'),
+                'name' => $anakategori->name
+            ];
+            $breadcrumb['active'] = $altkategori->name;
+        }
 
          $products = Product::whereStatus('1')->select(['id','name','slug','size','color','category_id','price','image'])
             ->where(function($q) use($sizes,$colors,$startprice,$endprice){
@@ -39,8 +63,6 @@ class PageController extends Controller
                 }
                 if(!empty($startprice) && $endprice)
                 {
-//                     $q->whereBetween('price',[$startprice,$endprice]);
-
                      $q->where('price','>=',$startprice);
                      $q->where('price','<=',$endprice);
                 }
@@ -66,12 +88,18 @@ class PageController extends Controller
 
             $colors = Product::whereStatus('1')->groupBy('color')->pluck('color')->toArray();
 
-        return view('frontend.pages.products',compact('products','maxprice','sizelists','colors'));
+        return view('frontend.pages.products',compact('breadcrumb','products','maxprice','sizelists','colors'));
     }
 
     public function inidirimliurunler()
     {
-        return view('frontend.pages.products');
+        $breadcrumb = [
+            'sayfalar' => [
+
+            ],
+            'active' => 'İndiirmli Ürünler',
+        ];
+        return view('frontend.pages.products',compact('breadcrumb'));
     }
 
     public function urundetay($slug)
@@ -83,17 +111,36 @@ class PageController extends Controller
             ->whereStatus('1')
             ->limit(6)
             ->get();
-        return view('frontend.pages.product_details',compact('product','products'));
+
+        $breadcrumb = [
+            'sayfalar' => [
+
+            ],
+            'active' => $product->name,
+        ];
+        return view('frontend.pages.product_details',compact('breadcrumb','product','products'));
     }
 
     public function hakkimizda()
     {
         $abouts = About::whereId('1')->first();
-        return view('frontend.pages.about',compact('abouts'));
+        $breadcrumb = [
+            'sayfalar' => [
+
+            ],
+            'active' => 'Hakkımızda',
+        ];
+        return view('frontend.pages.about',compact('breadcrumb','abouts'));
     }
 
     public function iletisim()
     {
-        return view('frontend.pages.contact');
+        $breadcrumb = [
+            'sayfalar' => [
+
+            ],
+            'active' => 'İletişim',
+        ];
+        return view('frontend.pages.contact',compact('breadcrumb'));
     }
 }
